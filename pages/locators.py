29bf71ebab4e0f0051ee5243c418894ec4e486c5
Andrey Negru <shiza1388@gmail.com>
2022-08-05 19:15:29 +0400

    Add parametrize test and mark one of them xfail.

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,13 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "[class='active']")
+    PRODUCT_NAME_IN_BASKET = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    PRODUCT_PRICE_IN_BASKET = (By.CSS_SELECTOR,
+                               "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > "
+                               "div > p:nth-child(1) > strong")
